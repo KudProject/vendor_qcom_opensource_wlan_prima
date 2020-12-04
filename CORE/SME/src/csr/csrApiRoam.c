e40b28913907787035c39a4e46a4880acc359a17
Albert I <kras@raphielgang.org>
2020-12-04 14:23:05 +0800

    prima: Don't warn on invalid session ID on csrRoamCallCallback()
    I'm not seeing any reason why this should be warned.
    
    Signed-off-by: Albert I <kras@raphielgang.org>

--- CORE/SME/src/csr/csrApiRoam.c
+++ CORE/SME/src/csr/csrApiRoam.c
@@ -2822,7 +2822,6 @@ eHalStatus csrRoamCallCallback(tpAniSirGlobal pMac, tANI_U32 sessionId, tCsrRoam
     else
     {
        smsLog(pMac, LOGE, "Session ID:%d is not valid", sessionId);
-       VOS_ASSERT(0);
        return eHAL_STATUS_FAILURE;
     }
 
